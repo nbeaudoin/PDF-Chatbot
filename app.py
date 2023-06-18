9579b1d4f28435c0498402a1a1f85f5e729ba6cd
Nicholas Beaudoin <nick.s.beaudoin@gmail.com>
2023-06-18 06:35:34 -0700

    Creaed requirements and app

---
+++ app.py
@@ -0,0 +1,17 @@
+#!pip install -r requirements.txt
+import streamlit as st
+
+
+
+def main():
+    st.set_page_config(page_title='Chat with multiple PDFs', page_icon=':books:')
+
+    st.header('Chat with multiple PDFs :books:')
+    
+
+
+
+
+
+if __name__ == '__main__':
+    main()
